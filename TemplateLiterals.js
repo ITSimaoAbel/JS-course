0b96a6e7d308a488842ba5cd189bbc9c9f095a4b
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-28 13:32:12 +0200

    Create Teplate Literals.js

---
+++ TemplateLiterals.js
@@ -0,0 +1,15 @@
+const nome = 'Maria';
+const idade = 30;
+const apresentacao = `Olá, meu nome é ${nome} e eu tenho ${idade} anos.`;
+console.log(apresentacao); 
+
+//Exemplo Multilinha
+const mensagem = `Esta é uma mensagem
+que ocupa várias linhas
+sem precisar de quebras de linha explícitas.`;
+console.log(mensagem);
+
+//Exemplo com Expressões
+const a = 5;
+const b = 10;
+console.log(`A soma de ${a} e ${b} é ${a + b}.`); // Saída: "A soma de 5 e 10 é 15."
