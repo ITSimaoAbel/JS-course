175d781e65e15ab9bade23d85aba7abe15c3dc04
ITSimaoAbel <simao3cuco@gmail.com>
2024-10-28 13:56:26 +0200

    Creat DestructuringAssignment.js

---
+++ DestructuringAssignment.js
@@ -0,0 +1,14 @@
+//Em Arrays
+const numeros = [1, 2, 3];
+const [primeiro, segundo] = numeros;
+
+console.log(primeiro);
+console.log(segundo);  
+
+
+//Em Objectos
+const pessoa = { nome: 'Ana', idade: 25 };
+const { nome, idade } = pessoa;
+
+console.log(nome); // Saída: "Ana"
+console.log(idade); // Saída: 25
